92e054dc1a899dfe810072c532908c343ff7ecc4
Alexsander Genuino <genuinoalexsander@gmail.com>
2021-10-15 15:31:45 -0300

    Estrutura

---
+++ Mevents_api/src/Api.Application/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Api.Service.Services;
+using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Interfaces;
+using Api.Data.Repository;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.AddDbContext<MyContext>(
+                options => options.UseMySql("Server=localhost;Port=3306;Database=Mevents_db;Uid=root;Pwd=root123")
+            );
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "application", Version = "v1" });
+            });
+
+            //Dependency Injection
+            services.AddScoped(typeof (IRepository<>), typeof(BaseRepository<>));
+            services.AddTransient<IUserService, UserService>();
+
+            
+            
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "application v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
